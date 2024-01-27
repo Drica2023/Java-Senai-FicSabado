39e30f8b8e240042b6a2e37ed7ba58a99833380c
Adriana <adrianamedeiros.dev@outlook.com>
2024-01-27 10:33:57 -0300

    estrutura repeticao for

---
+++ sabado27012024/src/estruturaRepeticao/EstruturaRepeticao.java
@@ -0,0 +1,42 @@
+package estruturaRepeticao;
+
+public class EstruturaRepeticao {
+
+	public static void main(String[] args) {
+
+		// for
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++)
+				System.out.print("*");
+			System.out.println();
+
+		}
+
+		for (int i = 0; i < 8; i++) {
+			
+			for (int j = 1; j < i; j++)
+			
+			System.out.print(j);
+			System.out.println();
+
+		}
+		
+		System.out.println();
+		
+		for (int i = 7; i > 1; i--) {
+			
+			for (int j = 1; j < i; j++)
+			
+			System.out.print(j);
+			System.out.println();
+
+		}
+		
+
+		// while
+
+		// do while
+
+	}
+}
